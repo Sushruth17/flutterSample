676870a53a2e411a6570d5128dc2c196641c19a1
sushruth17 <sushruth.sushe@gmail.com>
2021-03-24 16:16:37 +0530

    chat app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app1/pages/ConversationPage.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,39 +10,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Messio',
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
+        primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Messio'),
+      home: ConversationPage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
-        body: Center(
-            child:Text('Hello World!')
-        )
-    );
-
   }
 }
